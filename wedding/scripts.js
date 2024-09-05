2af6e8012d4fa5700cbc0e86336d179dd09267d6
yididiya-kassahun <yididiya127@gmail.com>
2024-09-05 06:11:44 +0300

    axios form added

--- wedding/scripts.js
+++ wedding/scripts.js
@@ -1,5 +1,34 @@
 const button = document.getElementById("startBtn");
 
-button.addEventListener('click', function(){
-    alert('The button clicked!!');
-})
+button.addEventListener("click", function () {
+  alert("The button clicked!!");
+});
+
+// send RSVP form data
+
+document
+  .getElementById("contactForm")
+  .addEventListener("submit", function (event) {
+    event.preventDefault(); // Prevent the form from submitting the default way
+
+    const formData = {
+      Name: document.getElementById("name").value,
+      NumofGuests: document.getElementById("no").value,
+      Email: document.getElementById("email").value,
+      Phone: document.getElementById("phone").value,
+      dietary: document.getElementById("dietary").value,
+      accomodation: document.getElementById("accomodation").value,
+      Message: document.getElementById("message").value,
+    };
+
+    axios
+      .post("/wedding-rsvp/<%= customer.OrderId %>", formData)
+      .then(function (response) {
+        console.log("Form submitted successfully", response);
+        alert("Form submitted successfully!");
+      })
+      .catch(function (error) {
+        console.error("There was an error submitting the form", error);
+        alert("There was an error submitting the form. Please try again.");
+      });
+  });
