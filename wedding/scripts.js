b1ed9a1e3553eb43224a0fd21b4292f602035e95
yididiya-kassahun <yididiya127@gmail.com>
2024-09-05 06:20:19 +0300

    js form logic modified

--- wedding/scripts.js
+++ wedding/scripts.js
@@ -11,6 +11,10 @@ document
   .addEventListener("submit", function (event) {
     event.preventDefault(); // Prevent the form from submitting the default way
 
+    // Extract the dynamic 'name' from the current URL
+    const path = window.location.pathname;
+    const name = path.split("/")[1]; // This assumes the URL structure is rsvp.inviteme.ai/name
+
     const formData = {
       Name: document.getElementById("name").value,
       NumofGuests: document.getElementById("no").value,
@@ -21,14 +25,13 @@ document
       Message: document.getElementById("message").value,
     };
 
+    // Post to the dynamic wedding-rsvp route
     axios
-      .post("/wedding-rsvp/<%= customer.OrderId %>", formData)
+      .post(`/wedding-rsvp/${name}`, formData)
       .then(function (response) {
-        console.log("Form submitted successfully", response);
-        alert("Form submitted successfully!");
+        alert("Form submitted successfully!"); // Success alert
       })
       .catch(function (error) {
-        console.error("There was an error submitting the form", error);
-        alert("There was an error submitting the form. Please try again.");
+        alert("There was an error submitting the form. Please try again."); // Error alert
       });
   });
