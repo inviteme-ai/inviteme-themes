8edf229fc89ccc50f87cfd91ffa88236bc8d0d79
yididiya-kassahun <yididiya127@gmail.com>
2024-09-06 09:45:48 +0300

    modified

--- wedding/scripts.js
+++ wedding/scripts.js
@@ -1,49 +1,52 @@
-// Add click listener to the submit button
-  document.getElementById('submitBtn').addEventListener('click', function(event) {
-    event.preventDefault(); 
-    handleSubmit(); 
-  });
+document.addEventListener("DOMContentLoaded", function () {
+  document
+    .getElementById("submitBtn")
+    .addEventListener("click", function (event) {
+      event.preventDefault();
+      handleSubmit();
+    });
+});
 
- function handleSubmit() {
-// send RSVP form data
+function handleSubmit() {
+  // send RSVP form data
 
-    // Extract the dynamic 'name' from the current URL
-    const path = window.location.pathname;
-    const name = path.split("/")[1]; 
+  // Extract the dynamic 'name' from the current URL
+  const path = window.location.pathname;
+  const name = path.split("/")[1];
 
-    const response = document.querySelector(
-      'input[name="response"]:checked'
-    ).value;
+  const response = document.querySelector(
+    'input[name="response"]:checked'
+  ).value;
 
-    const formData = {
-      Name: document.getElementById("name").value,
-      NumofGuests: document.getElementById("no").value,
-      Email: document.getElementById("email").value,
-      Phone: document.getElementById("phone").value,
-      dietary: document.getElementById("dietary").value,
-      accomodation: document.getElementById("accomodation").value,
-      Message: document.getElementById("message").value,
-      response: response,
-      subdomain: name, // Pass the extracted name as subdomain
-    };
-fetch(`/wedding-rsvp/${name}`, {
-  method: "POST",
-  headers: {
-    "Content-Type": "application/json",
-  },
-  body: JSON.stringify(formData),
-})
-  .then((response) => response.json())
-  .then((data) => {
-    // Handle the response from the server
-    if (data.success) {
-      alert("Form submitted successfully!");
-    } else {
-      alert("Error submitting form.");
-    }
+  const formData = {
+    Name: document.getElementById("name").value,
+    NumofGuests: document.getElementById("no").value,
+    Email: document.getElementById("email").value,
+    Phone: document.getElementById("phone").value,
+    dietary: document.getElementById("dietary").value,
+    accomodation: document.getElementById("accomodation").value,
+    Message: document.getElementById("message").value,
+    response: response,
+    subdomain: name, // Pass the extracted name as subdomain
+  };
+  fetch(`/wedding-rsvp/${name}`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(formData),
   })
-  .catch((error) => {
-    alert("There was an error submitting the form. Please try again.");
-    console.error(error);
-  });
+    .then((response) => response.json())
+    .then((data) => {
+      // Handle the response from the server
+      if (data.success) {
+        alert("Form submitted successfully!");
+      } else {
+        alert("Error submitting form.");
+      }
+    })
+    .catch((error) => {
+      alert("There was an error submitting the form. Please try again.");
+      console.error(error);
+    });
 }
